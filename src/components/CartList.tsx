4680dbc8660dd66ee2f5630190397ddeaba7e781
Cheulong Sear <searcheulong@gmail.com>
2021-11-28 18:32:04 +0700

    add style cartpage

--- src/components/CartList.tsx
+++ src/components/CartList.tsx
@@ -4,14 +4,14 @@ import { BookType } from 'src/types/Book.type';
 interface BookListProps {
   books: BookType[];
 }
-const BookList = ({ books }: BookListProps) => {
+const CartList = ({ books }: BookListProps) => {
   return (
     <div className={'w-10/12 m-auto mt-10'}>
-      <table className={'table-fixed shadow-lg bg-white m-auto w-4/5'}>
+      <table className={'table-fixed shadow-lg bg-white m-auto  w-4/5'}>
         <thead>
           <tr>
             <th className={'w-2/5  px-8 py-4'}>Title</th>
-            <th className={'w-1/5  px-8 py-4'}>Author</th>
+            <th className={'w-1/5  px-8 py-4'}>Quantity</th>
             <th className={'w-1/5  px-8 py-4'}>Price</th>
             <th className={'w-1/5  px-8 py-4'}></th>
           </tr>
@@ -20,14 +20,19 @@ const BookList = ({ books }: BookListProps) => {
           {books.map((book) => (
             <tr key={book.id}>
               <td className={'w-2/5 border px-8 py-4'}>{book.title}</td>
-              <td className={'w-1/5 border px-8 py-4'}>{book.author}</td>
+              <td className={'w-1/5 border px-8 py-4 leading-none'}>
+                <input
+                  className={'border-2 border-gray-200 w-full'}
+                  value={0}
+                />{' '}
+              </td>
               <td className={'w-1/5 border px-8 py-4'}>{'$' + book.price}</td>
               <button
                 className={
-                  ' w-full bg-blue-500 text-white hover:underline hover:bg-blue-600 px-8 py-4'
+                  ' w-full bg-red-500 text-white hover:underline hover:bg-red-600 px-8 py-4'
                 }
               >
-                Add
+                Remove
               </button>
             </tr>
           ))}
@@ -37,4 +42,4 @@ const BookList = ({ books }: BookListProps) => {
   );
 };
 
-export default BookList;
+export default CartList;
