37af9ccf1b446ded2806768392a47d0a26a2a213
Cheulong Sear <searcheulong@gmail.com>
2021-11-28 19:12:12 +0700

    add total price to cartpage

--- src/components/CartList.tsx
+++ src/components/CartList.tsx
@@ -23,19 +23,28 @@ const CartList = ({ books }: BookListProps) => {
               <td className={'w-1/5 border px-8 py-4 leading-none'}>
                 <input
                   className={'border-2 border-gray-200 w-full'}
-                  value={0}
+                  defaultValue={0}
                 />{' '}
               </td>
               <td className={'w-1/5 border px-8 py-4'}>{'$' + book.price}</td>
-              <button
-                className={
-                  ' w-full bg-red-500 text-white hover:underline hover:bg-red-600 px-8 py-4'
-                }
-              >
-                Remove
-              </button>
+              <td>
+                <button
+                  className={
+                    ' w-full bg-red-500 text-white hover:underline hover:bg-red-600 px-8 py-4'
+                  }
+                >
+                  Remove
+                </button>
+              </td>
             </tr>
           ))}
+          <tr>
+            <td />
+            <td />
+            <td colSpan={2} className={'border  px-8 py-4'}>
+              <p className={'font-bold'}>Total: $10</p>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
--- src/pages/users/[userId].tsx
+++ src/pages/users/[userId].tsx
@@ -3,24 +3,24 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/Home.module.css';
 import Users from '../../components/Users';
-import { Users as UserContents } from '../../contents/Books.content';
+// import { Users as UserContents } from '../../contents/Books.content';
 import { useRouter } from 'next/router';
-import { UserType } from '../../types/Book.type';
+// import { UserType } from '../../types/Book.type';
 
 const UserDetail: NextPage = () => {
   const router = useRouter();
   const { userId } = router.query;
-  const userDetail = UserContents.filter(
-    (user: UserType) => user.id.toString() == userId
-  );
+  // const userDetail = UserContents.filter(
+  //   (user: UserType) => user.id.toString() == userId
+  // );
   return (
     <>
       <button onClick={() => router.back()}>back</button>
       <div className={styles.container}>
-        <p>{userDetail[0].id}</p>
+        {/* <p>{userDetail[0].id}</p>
         <p>First Name: {userDetail[0].firstName}</p>
         <p>Last Name: {userDetail[0].lastName}</p>
-        <p>Age: {userDetail[0].age}</p>
+        <p>Age: {userDetail[0].age}</p> */}
       </div>
     </>
   );
